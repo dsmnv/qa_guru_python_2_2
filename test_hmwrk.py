02bdb229797ef0a5cff707afa936432a9d3d8e8f
dsmnv <dmitry.d.sem@gmail.com>
2022-08-31 23:52:15 +0300

    Добавлены фикстуры, позитивные и негативные тесты

--- test_hmwrk.py
+++ test_hmwrk.py
@@ -2,18 +2,41 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have, config
 
+config.hold_browser_open = True
 
 @pytest.fixture()
-def open_browser():
+def browser_config_fhd():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+@pytest.fixture()
+def browser_config_mobile():
+    browser.config.window_width = 390
+    browser.config.window_height = 844
+
+@pytest.fixture()
+def open_browser_fhd(browser_config_fhd):
     browser.open('https://google.com/ncr')
 
 @pytest.fixture()
-def resize_browser_fhd():
-    browser.config.window_width = 1920
-    browser.config.window_heightn = 1080
+def open_browser_mobile(browser_config_mobile):
+    browser.open('https://google.com/ncr')
 
-
-def test_positive(open_browser, resize_browser_fhd):
+# ПК
+def test_find_selene_positive(open_browser_fhd):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
+def test_find_selene_negative(open_browser_fhd):
+    browser.element('[name="q"]').should(be.blank).type('selenium').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
+
+# моб
+
+def test_find_selene_positive_mobile(open_browser_mobile):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_find_selene_negative_mobile(open_browser_mobile):
+    browser.element('[name="q"]').should(be.blank).type('selenum').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
