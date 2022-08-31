c6212bacd8e97c76a676c38a80676e2dc4437469
dsmnv <dmitry.d.sem@gmail.com>
2022-08-31 21:54:31 +0300

    Добавил requirments.txt, test_hmwrk.py

--- test_hmwrk.py
+++ test_hmwrk.py
@@ -1,7 +1,19 @@
+import pytest
 from selene.support.shared import browser
-from selene import be, have
+from selene import be, have, config
 
-def test_positive():
+
+@pytest.fixture()
+def open_browser():
     browser.open('https://google.com/ncr')
+
+@pytest.fixture()
+def resize_browser_fhd():
+    browser.config.window_width = 1920
+    browser.config.window_heightn = 1080
+
+
+def test_positive(open_browser, resize_browser_fhd):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
