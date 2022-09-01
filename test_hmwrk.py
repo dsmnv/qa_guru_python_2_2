7e2e8e21582d89d51e3836a3b8c81905cc6fbc41
dsmnv <dmitry.d.sem@gmail.com>
2022-09-01 22:06:11 +0300

    Изменен негативный тест

--- test_hmwrk.py
+++ test_hmwrk.py
@@ -1,12 +1,12 @@
 from selene.support.shared import browser
 from selene import be, have
 
-
+search = 'biburibaba'
 def test_find_selene_positive(open_browser_fhd):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 def test_find_selene_negative(open_browser_fhd):
-    browser.element('[name="q"]').should(be.blank).type('selenium').press_enter()
-    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
-
+    browser.element('[name="q"]').should(be.blank).type(search).press_enter()
+    browser.element('[id="topstuff"]').should(be.visible)
+    browser.element('[id="topstuff"]').should(have.text(search))
